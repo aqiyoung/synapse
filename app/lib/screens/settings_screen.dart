105041912ffc85294f344bf29145dc25ddba39c5
aqiyoung <aqiyoung@users.noreply.github.com>
2026-05-30 18:08:42 +0800

    chore: 更新版本号 1.2.2+24

--- app/lib/screens/settings_screen.dart
+++ app/lib/screens/settings_screen.dart
@@ -110,7 +110,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ListTile(
             leading: Icon(Icons.info_outline, color: colorScheme.primary),
             title: const Text('版本'),
-            subtitle: const Text('1.2.1'),
+            subtitle: const Text('1.2.2'),
             contentPadding: EdgeInsets.zero,
           ),
           ListTile(
