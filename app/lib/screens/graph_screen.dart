5e4ddc0c3784f73f8a261e6605e5c05a0597882c
aqiyoung <aqiyoung@users.noreply.github.com>
2026-06-25 17:06:41 +0800

    fix: round 4 lint fixes (unused params, fields, const)

--- app/lib/screens/graph_screen.dart
+++ app/lib/screens/graph_screen.dart
@@ -129,8 +129,6 @@ class _GraphNode {
   final List<String> tags;
   Color color;
   double x, y;
-  double vx, vy;
-  int degree;
 
   _GraphNode({
     required this.id,
@@ -139,12 +137,9 @@ class _GraphNode {
     required this.color,
     required this.x,
     required this.y,
-    this.vx = 0,
-    this.vy = 0,
-    this.degree = 0,
+
   });
 
-  double get radius => 3 + (degree / 1.0).clamp(0, 1) * 13;
 }
 
 class _GraphWidget extends StatefulWidget {
